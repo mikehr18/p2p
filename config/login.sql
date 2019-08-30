7f7af198cc3e7c5dc5222b1552b1b7b2602f91ba
mikehr18 <mahr181196@gmail.com>
2019-08-30 13:42:02 -0500

    subí base de datos

--- config/login.sql
+++ config/login.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
--- http://www.phpmyadmin.net
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-09-2016 a las 20:38:28
--- Versión del servidor: 5.6.26
--- Versión de PHP: 5.6.12
+-- Tiempo de generación: 30-08-2019 a las 20:05:16
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,25 +25,63 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `archivos`
+--
+
+CREATE TABLE `archivos` (
+  `id_archivo` int(11) NOT NULL,
+  `nombre` text NOT NULL,
+  `peso` varchar(10) NOT NULL,
+  `id_login` int(11) NOT NULL,
+  `activo` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `archivos`
+--
+
+INSERT INTO `archivos` (`id_archivo`, `nombre`, `peso`, `id_login`, `activo`) VALUES
+(1, 'FB_IMG_1567179641967.jpg', '128.706054', 1, 1),
+(2, 'FB_IMG_1567179641967.jpg', '128.706054', 1, 1),
+(3, 'FB_IMG_1567179641967.jpg', '128.706054', 1, 1),
+(4, 'FB_IMG_1567179641967.jpg', '128.706054', 1, 1),
+(5, 'IMG_2019-08-30-07224611.jpg', '26.2197265', 1, 1),
+(8, 'FB_IMG_1567179641967.jpg', '128.706054', 2, 0),
+(9, 'FB_IMG_1567135992671.jpg', '36.0595703', 3, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `login`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
+CREATE TABLE `login` (
   `id` int(10) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `password` varchar(255) NOT NULL,
+  `ip` varchar(16) NOT NULL,
+  `activo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `login`
 --
 
-INSERT INTO `login` (`id`, `email`, `password`) VALUES
-(1, 'admin@admin.com', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO `login` (`id`, `email`, `password`, `ip`, `activo`) VALUES
+(1, 'admin@admin.com', '4c3e1ec04215f69d6a8e9c023c9e4572', '192.168.137.152', 0),
+(2, 'mike', '4c3e1ec04215f69d6a8e9c023c9e4572', '192.168.137.152', 0),
+(3, 'pipe', '20826a3cb51d6c7d9c219c7f4bf4e5c9', '192.168.137.152', 0);
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD PRIMARY KEY (`id_archivo`),
+  ADD KEY `id_login` (`id_login`);
 
 --
 -- Indices de la tabla `login`
@@ -54,10 +94,28 @@ ALTER TABLE `login`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  MODIFY `id_archivo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT de la tabla `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `archivos`
+--
+ALTER TABLE `archivos`
+  ADD CONSTRAINT `archivos_ibfk_1` FOREIGN KEY (`id_login`) REFERENCES `login` (`id`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
